3bf2bd30e3c2537ceb501a306ed9317e2ae4feae
Larissa Mendes <83883968+annalare@users.noreply.github.com>
2022-09-18 19:28:47 -0300

    style: box

--- src/assets/App.styles.ts
+++ src/assets/App.styles.ts
@@ -34,7 +34,7 @@ export const Wrapper = styled.div`
   }
   h1 {
     font-family: Fascinate Inline;
-    background-image: linear-gradient(180deg, #fff, #87f1ff);
+    background-image: linear-gradient(180deg, #fff, #87ff);
     font-weight: 400;
     background-size: 100%;
     background-clip: text;
@@ -42,7 +42,7 @@ export const Wrapper = styled.div`
     -webkit-text-fill-color: transparent;
     -moz-background-clip: text;
     -moz-text-fill-color: transparent;
-    filter: drop-shadow(2px 2px #0085a3);
+    filter: drop-shadow(2px 2px rgb(164, 98, 226));
     font-size: 70px;
     text-align: center;
     margin: 20px;
@@ -50,9 +50,9 @@ export const Wrapper = styled.div`
   .start,
   .next {
     cursor: pointer;
-    background: linear-gradient(180deg, #ffffff, #ffcc91);
-    border: 2px solid #d38558;
-    box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.25);
+    background: linear-gradient(180deg, #ffffff, rgb(167, 76, 132));
+    border: 2px solidrgb(224, 22, 147);
+    box-shadow: 0px 5px 10px rgba(224, 20, 140, 0.25);
     border-radius: 10px;
     height: 40px;
     margin: 20px 0;
